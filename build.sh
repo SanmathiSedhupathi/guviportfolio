62f0fe61d4e92790e2c4b5b812a2c2ee6076ffcd
S.SANMATHI <127740223+SanmathiSedhupathi@users.noreply.github.com>
2025-02-06 12:06:18 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -12,6 +12,10 @@ echo "Starting Deployment Script"
 echo "Installing npm and dependencies..."
 apt-get update && apt-get install -y nodejs npm
 npm install
+apt update
+apt install -y curl
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
 
 # Step 2: Build React App
 echo "Building React Application..."
