c0c21ee7b1b136082928b4f0ceadc349965a6d80
S.SANMATHI <127740223+SanmathiSedhupathi@users.noreply.github.com>
2025-02-06 13:05:40 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -16,13 +16,13 @@ npm run build
 
 # Step 2: Build Docker Image
 echo "Building Docker Image..."
-docker build -t sanmathisedhupathi/test:latest ..
+docker build -t sanmathisedhupathi/myportfolio:latest ..
 
 # Step 3: Push Docker Image to Docker Hub
 echo "Pushing Docker Image to Docker Hub..."
 docker login -u sanmathisedhupathi -p 08-Sep-2004
 
-docker push sanmathisedhupathi/test:latest
+docker push sanmathisedhupathi/myportfolio:latest
 
 
 # Check if kubectl is installed, if not, install it
